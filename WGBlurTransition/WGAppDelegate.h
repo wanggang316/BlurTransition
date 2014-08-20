ea75942330ecc23294969286a11c3de07a38d8d1
wanggang <wgang316@163.com>
2014-08-20 10:28:23 +0800

    Initial Commit

---
+++ WGBlurTransition/WGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WGAppDelegate.h
+//  WGBlurTransition
+//
+//  Created by 王刚 on 14/8/20.
+//  Copyright (c) 2014年 wwwlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WGBlurTransition/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WGBlurTransition
+//
+//  Created by 王刚 on 14/8/20.
+//  Copyright (c) 2014年 wwwlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WGAppDelegate class]));
+    }
+}
